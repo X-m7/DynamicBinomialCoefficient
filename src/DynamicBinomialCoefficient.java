6cf2043b9f5b4cb9ae96d8358310f21bfd743dfc
Prajna Sariputra <putr4.s@gmail.com>
2018-10-12 13:46:48 +1100

    Initial algorithm (currently has overflow for n > 20)

--- src/DynamicBinomialCoefficient.java
+++ src/DynamicBinomialCoefficient.java
@@ -1,9 +1,36 @@
+import java.util.Scanner;
 
 public class DynamicBinomialCoefficient {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input: ");
+		String inputN = input.next();
+		String inputK = input.next();
+		try {
+			int n = Integer.parseInt(inputN);
+			int k = Integer.parseInt(inputK);
+			if (n < 0) {
+				throw new NumberFormatException();
+			}
+			System.out.println(c(n, k));
+		} catch (NumberFormatException e) {
+			System.out.println("Invalid input");
+		} finally {
+			input.close();
+		}
+	}
+	
+	public static long c(int n, int k) {
+		if (n == 0) {
+			return 0L;
+		}
+		long[] factorialResults = new long[n + 1];
+		factorialResults[0] = 1; //0! = 1
+		for (int counter = 1; counter <= n; counter++) { //calculate n! and all before
+			factorialResults[counter] = (long) counter * factorialResults[counter - 1];
+		}
+		return factorialResults[n] / (factorialResults[k] * factorialResults[n - k]);
 	}
 
 }
